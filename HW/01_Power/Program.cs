08637c26d50e1e9eb84925584025f01337dcf832
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-16 23:29:35 +0300

    add HW 1 Power

--- HW/01_Power/Program.cs
+++ HW/01_Power/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит
+// число A в натуральную степень B.
+using System;
+using static System.Console;
+
+Clear();
+int A, B;
+Write("Введите число A: ");
+if (!int.TryParse(ReadLine(), out A)) {
+	WriteLine("Ошибка ввода числа!");
+	return;
+}
+Write("Введите число B: ");
+if (!int.TryParse(ReadLine(), out B)) {
+	WriteLine("Ошибка ввода числа!");
+	return;
+}
+if (B < 1) {
+	WriteLine("ОШИБКА: число В не натуральное!");
+	return;
+}
+
+Write($"{A}^{B} = {Power(A, B)}");
+
+int Power (int a, int b) {
+	int result = 1;
+	for (int i = 0; i < b; i ++) {
+		result *= a;
+	}
+	return result;
+}
